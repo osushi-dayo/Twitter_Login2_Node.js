de8c83696714d08e894f72f7d5c1d64f539247ea
atsushi otsubo <sayurin@atsushi-no-MacBook-Pro.local>
2014-12-09 05:54:41 +0900

    initial commit

---
+++ dammy_settings.js
@@ -0,0 +1,19 @@
+//アプリケーションのホストとポート
+exports.host = "127.0.0.1";
+exports.port = "3000";
+
+//セッション管理にデータベースを使用。
+//mongodが起動していれば.存在しない名前でも自動で新規作成してくれる。
+exports.db_host = "127.0.0.1";// データベースのホスト
+exports.db_port = 6379;
+
+//Twitterのトークン
+//アクセストークンはsignin_with_twitterのみの場合は不要
+exports.CONSUMER_KEY =        'YOUR_CONSUMER_KEY';
+exports.CONSUMER_SECRET =     'YOUR_CONSUMER_SECRET';
+exports.ACCESS_TOKEN_KEY =    'YOUR_ACCESS_TOKEN_KEY';
+exports.ACCESS_TOKEN_SECRET = 'YOUR_ACCESS_TOKEN_SECRET';
+
+//推測されない文字列
+exports.session_key_secret = "RANDOM_STR";
+
